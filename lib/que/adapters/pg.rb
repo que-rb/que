0b379c848399390c70e5fbd98792b62ef582b80e
Chris Hanks <christopher.m.hanks@gmail.com>
2013-11-13 00:23:36 +0000

    Suppress annoying NOTICEs to STDOUT when using a bare PG connection.

--- lib/que/adapters/pg.rb
+++ lib/que/adapters/pg.rb
@@ -2,6 +2,7 @@ module Que
   class PG < Adapter
     def initialize(pg)
       @pg = pg
+      execute "SET client_min_messages TO 'warning'" # Avoid annoying NOTICE messages.
     end
 
     def execute(sql)
