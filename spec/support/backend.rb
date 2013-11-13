dcf0f8aaf8a45ea5be82b3de8b57e9f9580feab1
Chris Hanks <christopher.m.hanks@gmail.com>
2013-11-13 22:20:43 +0000

    Add some pending specs on queueing jobs.

--- spec/support/backend.rb
+++ spec/support/backend.rb
@@ -13,4 +13,34 @@ shared_examples "a Que backend" do
     Que.clear!
     DB[:que_jobs].count.should be 0
   end
+
+  describe "when queueing jobs" do
+    it "should be able to queue a job with arguments" do
+      pending
+    end
+
+    it "should be able to queue a job with a specific time to run" do
+      pending
+    end
+
+    it "should be able to queue a job with a specific priority" do
+      pending
+    end
+
+    it "should be able to queue a job with queueing options in addition to argument options" do
+      pending
+    end
+
+    it "should respect a default (but overridable) priority for the job class" do
+      pending
+    end
+
+    it "should respect a default (but overridable) run_at for the job class" do
+      pending
+    end
+
+    it "should raise an error if given arguments that can't convert to and from JSON unambiguously" do
+      pending
+    end
+  end
 end
