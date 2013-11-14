214c4dcb1fb14364dcba3e4ff6bfac3a0654514a
Chris Hanks <christopher.m.hanks@gmail.com>
2013-11-14 04:44:58 +0000

    Only suppress NOTICE messages when we're actually creating the table.

--- lib/que/adapters/pg.rb
+++ lib/que/adapters/pg.rb
@@ -3,7 +3,6 @@ module Que
     def initialize(pg)
       @pg    = pg
       @mutex = Mutex.new
-      execute "SET client_min_messages TO 'warning'" # Avoid annoying NOTICE messages.
     end
 
     def checkout
--- spec/support/backend.rb
+++ spec/support/backend.rb
@@ -3,6 +3,7 @@ shared_examples "a Que backend" do
     DB.table_exists?(:que_jobs).should be true
     Que.drop!
     DB.table_exists?(:que_jobs).should be false
+    Que.execute "SET client_min_messages TO 'warning'" # Avoid annoying NOTICE messages.
     Que.create!
     DB.table_exists?(:que_jobs).should be true
   end
