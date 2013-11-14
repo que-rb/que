424f8bb1e74302d130fbac52547a888bf8b14ec5
Chris Hanks <christopher.m.hanks@gmail.com>
2013-11-14 06:48:15 +0000

    Make sure that the same connection is used throughout locking a job, working it, and releasing the lock.

--- lib/que/job.rb
+++ lib/que/job.rb
@@ -31,28 +31,35 @@ module Que
       # that there may be work available. In a work loop, we'd want to sleep
       # for a while only if Job.work returned falsy.
       def work
-        if row = Que.execute(LockSQL).first
-          # Edge case: It's possible for the lock statement to have grabbed a
-          # job that's already been worked, if the statement took its MVCC
-          # snapshot while the job was processing (making it appear to still
-          # exist), but didn't actually attempt to lock it until the job was
-          # finished (making it appear to be unlocked). Now that we have the
-          # job lock, we know that a previous worker would have deleted it by
-          # now, so we just check that it still exists before working it. Note
-          # that there is currently no spec for this behavior, since I'm not
-          # sure how to deterministically commit a transaction that deletes a
-          # job between these two queries.
-          check = Que.execute "SELECT 1 AS one FROM que_jobs WHERE priority = $1 AND run_at = $2 AND job_id = $3;", [row['priority'], row['run_at'], row['job_id']]
-          return true if check.none?
+        # Since we're taking session-level advisory locks, we have to hold the
+        # same connection throughout the process of getting a job, working it,
+        # deleting it, and removing the lock.
+        Que.connection.checkout do
+          begin
+            if row = Que.execute(LockSQL).first
+              # Edge case: It's possible for the lock statement to have grabbed a
+              # job that's already been worked, if the statement took its MVCC
+              # snapshot while the job was processing (making it appear to still
+              # exist), but didn't actually attempt to lock it until the job was
+              # finished (making it appear to be unlocked). Now that we have the
+              # job lock, we know that a previous worker would have deleted it by
+              # now, so we just check that it still exists before working it. Note
+              # that there is currently no spec for this behavior, since I'm not
+              # sure how to deterministically commit a transaction that deletes a
+              # job between these two queries.
+              check = Que.execute "SELECT 1 AS one FROM que_jobs WHERE priority = $1 AND run_at = $2 AND job_id = $3;", [row['priority'], row['run_at'], row['job_id']]
+              return true if check.none?
 
-          job = const_get(row['type']).new
-          job.run(*JSON.load(row['args']))
-          Que.execute "DELETE FROM que_jobs WHERE priority = $1 AND run_at = $2 AND job_id = $3", [row['priority'], row['run_at'], row['job_id']]
+              job = const_get(row['type']).new
+              job.run(*JSON.load(row['args']))
+              Que.execute "DELETE FROM que_jobs WHERE priority = $1 AND run_at = $2 AND job_id = $3", [row['priority'], row['run_at'], row['job_id']]
 
-          job
+              job
+            end
+          ensure
+            Que.execute "SELECT pg_advisory_unlock($1)", [row['job_id']] if row
+          end
         end
-      ensure
-        Que.execute "SELECT pg_advisory_unlock($1)", [row['job_id']] if row
       end
 
       private
