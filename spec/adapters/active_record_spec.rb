975ac458310a9059247f7daa3e62eafcaab9403b
Chris Hanks <christopher.m.hanks@gmail.com>
2013-11-17 04:21:39 +0000

    Add a couple of specs to make sure that the ORMs' connections are shared as they should be.

--- spec/adapters/active_record_spec.rb
+++ spec/adapters/active_record_spec.rb
@@ -10,4 +10,18 @@ describe "Que using ActiveRecord's connection" do
 
   it_behaves_like "a Que adapter"
   it_behaves_like "a multithreaded Que adapter"
+
+  it "should use the same connection that ActiveRecord does" do
+    class ActiveRecordJob < Que::Job
+      def run
+        $pid1 = Que.execute("SELECT pg_backend_pid()").first['pg_backend_pid'].to_i
+        $pid2 = ActiveRecord::Base.connection.select_all("select pg_backend_pid()").rows.first.first.to_i
+      end
+    end
+
+    ActiveRecordJob.queue
+    Que::Job.work
+
+    $pid1.should == $pid2
+  end
 end
--- spec/adapters/sequel_spec.rb
+++ spec/adapters/sequel_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-Que.connection = Sequel.connect(QUE_URL)
+Que.connection = SEQUEL_ADAPTER_DB = Sequel.connect(QUE_URL)
 QUE_ADAPTERS[:sequel] = Que.adapter
 
 describe "Que using a Sequel database's connection" do
@@ -8,4 +8,18 @@ describe "Que using a Sequel database's connection" do
 
   it_behaves_like "a Que adapter"
   it_behaves_like "a multithreaded Que adapter"
+
+  it "should use the same connection that Sequel does" do
+    class SequelJob < Que::Job
+      def run
+        $pid1 = Que.execute("SELECT pg_backend_pid()").first['pg_backend_pid'].to_i
+        $pid2 = SEQUEL_ADAPTER_DB.get{pg_backend_pid{}}
+      end
+    end
+
+    SequelJob.queue
+    Que::Job.work
+
+    $pid1.should == $pid2
+  end
 end
