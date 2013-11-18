37273a2c33fd4700f697182a7f2515e77d973189
Chris Hanks <christopher.m.hanks@gmail.com>
2013-11-18 22:02:55 +0000

    Don't load the worker pool when running Rails console, runner, etc. Only the server.

--- lib/que/railtie.rb
+++ lib/que/railtie.rb
@@ -2,15 +2,18 @@ module Que
   class Railtie < Rails::Railtie
     config.que = Que
     config.que.connection = ::ActiveRecord if defined?(::ActiveRecord)
+    config.que.mode = :sync if Rails.env.test?
 
     rake_tasks do
       load 'que/rake_tasks.rb'
     end
 
     initializer "que.setup" do
-      ActiveSupport.on_load(:active_record) do
+      ActiveSupport.on_load(:after_initialize) do
         Que.logger ||= Rails.logger
-        Que.mode ||= Rails.env.test? ? :sync : :async
+
+        # Only start up the worker pool if running as a server.
+        Que.mode ||= :async if defined? Rails::Server
       end
     end
   end
