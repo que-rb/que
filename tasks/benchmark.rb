1705045abe027e3a8da8eb2458e40b0fba91fdfa
Chris Hanks <christopher.m.hanks@gmail.com>
2013-12-16 22:16:42 +0000

    Revert "No need for a separate tasks folder now, really."
    This reverts commit 2147fe8273ed95d425228fab751702d86eed157c.

---
+++ tasks/benchmark.rb
@@ -0,0 +1,1 @@
+# The benchmarking tasks have been merged together and now live at github.com/chanks/queue-shootout.
---
+++ tasks/rspec.rb
@@ -0,0 +1,12 @@
+require 'rspec/core/rake_task'
+
+RSpec::Core::RakeTask.new :default do |spec|
+  spec.pattern = './spec/**/*_spec.rb'
+end
+
+# Shortcut to skip the adapter specs, and run only with the basic PG
+# connection. I use this occasionally to make sure ActiveRecord isn't loaded,
+# so any accidental Rails-isms are caught.
+RSpec::Core::RakeTask.new :pg do |spec|
+  spec.pattern = './spec/unit/*_spec.rb'
+end
