7f539349ca17dc42f0db0fa0be2ab4a5b014fc1f
Chris Hanks <christopher.m.hanks@gmail.com>
2013-12-27 07:56:33 +0000

    Have Que.worker_count= return the new worker count.

--- lib/que/worker.rb
+++ lib/que/worker.rb
@@ -123,6 +123,8 @@ module Que
         elsif count < worker_count
           workers.pop(worker_count - count).each(&:stop).each(&:wait_until_stopped)
         end
+
+        worker_count
       end
 
       def worker_count
