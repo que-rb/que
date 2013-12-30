30256da9db059d5770f19e101194eaaf58df6b72
Chris Hanks <christopher.m.hanks@gmail.com>
2013-12-30 22:37:14 +0000

    Drop the specs on Que.stop! interacting with ORMs.
    They don't test anything that the safe_shutdown task can't, and making them reliable will be the death of me.

--- spec/adapters/sequel_spec.rb
+++ spec/adapters/sequel_spec.rb
@@ -22,34 +22,4 @@ describe "Que using the Sequel adapter" do
 
     $pid1.should == $pid2
   end
-
-  it "should safely roll back in-process transactions when using Que.stop!" do
-    begin
-      class SequelInterruptJob < BlockJob
-        def run
-          SEQUEL_ADAPTER_DB.transaction do
-            Que.execute "INSERT INTO que_jobs (job_id, job_class) VALUES (0, 'Que::Job')"
-            super
-          end
-        end
-      end
-
-      SequelInterruptJob.queue
-      Que.mode = :async
-      $q1.pop
-      Que.stop!
-
-      DB[:que_jobs].where(:job_id => 0).should be_empty
-    ensure
-      # Que.stop! can affect DB connections in an unpredictable fashion, and
-      # Sequel's built-in reconnection logic may not be able to recover them.
-      # So, force a reconnection for the sake of the other specs...
-      SEQUEL_ADAPTER_DB.disconnect
-
-      # ...and that's not even foolproof, because threads may have died with
-      # connections checked out.
-      SEQUEL_ADAPTER_DB.pool.allocated.each_value(&:close)
-      SEQUEL_ADAPTER_DB.pool.allocated.clear
-    end
-  end
 end
