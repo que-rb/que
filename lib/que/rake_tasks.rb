f6328f29a002f6f10740c0397b64d25918387453
Chris Hanks <christopher.m.hanks@gmail.com>
2014-01-06 02:00:31 +0000

    Now that Que.worker_count= automatically sets the mode to :async, we don't need to start with 4 workers when running the que:work rake task.

--- lib/que/rake_tasks.rb
+++ lib/que/rake_tasks.rb
@@ -4,7 +4,6 @@ namespace :que do
     require 'logger'
 
     Que.logger       = Logger.new(STDOUT)
-    Que.mode         = :async
     Que.worker_count = (ENV['WORKER_COUNT'] || 4).to_i
 
     # When changing how signals are caught, be sure to test the behavior with
