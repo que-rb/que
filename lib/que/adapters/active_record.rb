24a958868cea5cbba4fcb9bacf31af96b190c6c3
Joe Van Dyk <joe@fixieconsulting.com>
2014-01-13 02:21:27 -0800

    first pass at waking threads after job queue with ActiveRecord

--- lib/que/adapters/active_record.rb
+++ lib/que/adapters/active_record.rb
@@ -2,7 +2,31 @@ module Que
   module Adapters
     class ActiveRecord < Base
       def checkout
-        ::ActiveRecord::Base.connection_pool.with_connection { |conn| yield conn.raw_connection }
+        ::ActiveRecord::Base.connection_pool.with_connection do |conn|
+          @conn = conn
+          yield @conn.raw_connection
+        end
+      end
+
+      def wake_worker_after_commit
+        # Works with ActiveRecord 3.2 and 4 (possibly earlier, didn't check)
+        if @conn.raw_connection.transaction_status != PGconn::PQTRANS_IDLE
+          @conn.add_transaction_record(CommittedCallback.new)
+        else
+          Que.wake!
+        end
+      end
+
+      class CommittedCallback
+        def has_transactional_callbacks?
+          true
+        end
+        def logger
+          Logger.new(STDOUT) # for debugging
+        end
+        def committed!
+          Que.wake!
+        end
       end
     end
   end
