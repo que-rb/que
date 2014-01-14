615110f2053094437f158e515ef62e1c50f6abdd
Chris Hanks <christopher.m.hanks@gmail.com>
2014-01-14 23:09:14 +0000

    Make ActiveRecord worker wakeups thread-safe.

--- lib/que/adapters/active_record.rb
+++ lib/que/adapters/active_record.rb
@@ -2,16 +2,13 @@ module Que
   module Adapters
     class ActiveRecord < Base
       def checkout
-        ::ActiveRecord::Base.connection_pool.with_connection do |conn|
-          @conn = conn
-          yield @conn.raw_connection
-        end
+        checkout_activerecord_adapter { |conn| yield conn.raw_connection }
       end
 
       def wake_worker_after_commit
         # Works with ActiveRecord 3.2 and 4 (possibly earlier, didn't check)
-        if @conn.raw_connection.transaction_status != PGconn::PQTRANS_IDLE
-          @conn.add_transaction_record(CommittedCallback.new)
+        if in_transaction?
+          checkout_activerecord_adapter { |adapter| adapter.add_transaction_record(CommittedCallback.new) }
         else
           Que.wake!
         end
@@ -21,12 +18,16 @@ module Que
         def has_transactional_callbacks?
           true
         end
-        def logger
-          Logger.new(STDOUT) # for debugging
-        end
+
         def committed!
           Que.wake!
         end
+      end
+
+      private
+
+      def checkout_activerecord_adapter(&block)
+        ::ActiveRecord::Base.connection_pool.with_connection(&block)
       end
     end
   end
