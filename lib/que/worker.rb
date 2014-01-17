410dfccceed3869cd165de62e2aa345d55e3b817
Chris Hanks <christopher.m.hanks@gmail.com>
2014-01-17 03:41:53 +0000

    Add a very simple Worker class that runs jobs from a SortedQueue and returns results to a ThreadSafeArray.

--- lib/que/worker.rb
+++ lib/que/worker.rb
@@ -1,4 +1,19 @@
 module Que
   class Worker
+    def initialize(options)
+      @job_queue    = options[:job_queue]
+      @result_queue = options[:result_queue]
+      @thread       = Thread.new { work_loop }
+    end
+
+    private
+
+    def work_loop
+      loop do
+        job = @job_queue.shift
+        job._run
+        @result_queue.push job.attrs[:job_id]
+      end
+    end
   end
 end
--- spec/unit/worker_spec.rb
+++ spec/unit/worker_spec.rb
@@ -1,4 +1,40 @@
 require 'spec_helper'
 
 describe Que::Worker do
+  before do
+    @job_queue    = Que::SortedQueue.new
+    @result_queue = Que::ThreadSafeArray.new
+
+    @worker = Que::Worker.new :job_queue    => @job_queue,
+                              :result_queue => @result_queue
+  end
+
+  it "should repeatedly work jobs that are passed to it via its job_queue, ordered correctly" do
+    begin
+      $results = []
+
+      class WorkerJob < Que::Job
+        def run(number)
+          $results << number
+        end
+      end
+
+      jobs = [1, 2, 3].map do |i|
+        WorkerJob.new :priority => i,
+                      :run_at   => Time.now,
+                      :job_id   => i,
+                      :args     => "[#{i}]"
+      end
+
+      jobs.shuffle!
+
+      @job_queue.insert(jobs)
+      sleep_until { $results.count == 3 }
+
+      $results.should == [1, 2, 3]
+      @result_queue.to_a.should == [1, 2, 3]
+    ensure
+      $results = nil
+    end
+  end
 end
