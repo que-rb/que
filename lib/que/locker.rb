9e8f5c1d112ea4cf7a62ed56d4892d0e0b32f1a9
Chris Hanks <christopher.m.hanks@gmail.com>
2014-02-06 16:23:53 -0800

    Clean up Locker somewhat.

--- lib/que/locker.rb
+++ lib/que/locker.rb
@@ -5,11 +5,13 @@ module Que
   class Locker
     attr_reader :thread, :workers, :job_queue
 
-    def initialize(options = {})
-      @queue_name    = options[:queue] || ''
-      @listening     = !!options[:listening]
-      @poll_interval = options[:poll_interval] || 5
+    # When in the wait loop, wake up at least every 10 ms to check what's going on.
+    WAIT_PERIOD = 0.01
 
+    def initialize(options = {})
+      @listening          = !!options[:listening]
+      @queue_name         = options[:queue]              || ''
+      @poll_interval      = options[:poll_interval]      || 5
       @minimum_queue_size = options[:minimum_queue_size] || 2
 
       @locks        = Set.new
@@ -48,9 +50,13 @@ module Que
           Que.execute :clean_lockers
           Que.execute :register_locker, [@queue_name, @workers.count, Process.pid, Socket.gethostname, @listening]
 
+          poll
+
           loop do
-            poll
             wait
+            unlock_finished_jobs
+
+            poll if queue_refill_needed? || poll_interval_elapsed?
             break if @stop
           end
 
@@ -61,47 +67,47 @@ module Que
 
           unlock_finished_jobs
         ensure
-          Que.execute "UNLISTEN *"
           Que.execute "DELETE FROM que_lockers WHERE pid = $1", [backend_pid]
-          Que.adapter.drain_notifications
+
+          if @listening
+            Que.execute "UNLISTEN *"
+            Que.adapter.drain_notifications
+          end
         end
       end
     end
 
     private
 
-    # When waiting, wake up every 10 ms to check what's going on.
-    SLEEP_PERIOD = 0.01
+    def poll
+      count = @job_queue.space
+      jobs  = Que.execute :poll_jobs, [@queue_name, "{#{@locks.to_a.join(',')}}", count]
+
+      @locks.merge jobs.map { |pk| pk[:job_id] }
+      push_jobs *jobs
+
+      @last_polled_at      = Time.now
+      @last_poll_satisfied = count == jobs.count
+    end
 
     def wait
-      loop do
-        if @listening
-          if pk = Que.adapter.wait_for_json(SLEEP_PERIOD)
-            pk['run_at'] = Time.parse(pk['run_at'])
+      if @listening
+        if pk = Que.adapter.wait_for_json(WAIT_PERIOD)
+          pk['run_at'] = Time.parse(pk['run_at'])
 
-            push_jobs(pk) if @job_queue.accept?(pk) && lock_job?(pk[:job_id])
-          end
-        else
-          sleep SLEEP_PERIOD
+          push_jobs(pk) if @job_queue.accept?(pk) && lock_job?(pk[:job_id])
         end
-
-        unlock_finished_jobs
-        break if stop_waiting?
+      else
+        sleep WAIT_PERIOD
       end
     end
 
-    def stop_waiting?
-      if @stop
-        # Bail if the locker should be stopping.
-        true
-      elsif @last_poll_satisfied
-        # If the last poll returned all the jobs we asked it to, assume
-        # there's a backlog and go back for more when the queue runs low.
-        @job_queue.size <= @minimum_queue_size
-      else
-        # There's no backlog, so we wait until the poll_interval has elapsed.
-        time_until_next_poll.zero?
-      end
+    def queue_refill_needed?
+      @last_poll_satisfied && @job_queue.size <= @minimum_queue_size
+    end
+
+    def poll_interval_elapsed?
+      (Time.now - @last_polled_at) > @poll_interval
     end
 
     def lock_job?(id)
@@ -115,32 +121,18 @@ module Que
       unlock_jobs @result_queue.clear
     end
 
-    def unlock_jobs(ids)
-      ids.each do |id|
-        Que.execute "SELECT pg_advisory_unlock($1)", [id]
-        @locks.delete(id)
-      end
-    end
-
     def push_jobs(*pks)
+      # Unlock any low-importance jobs the new ones may displace.
       if ids = @job_queue.push(pks)
         unlock_jobs(ids)
       end
     end
 
-    def poll
-      count = @job_queue.space
-      jobs = Que.execute(:poll_jobs, [@queue_name, "{#{@locks.to_a.join(',')}}", count])
-      @locks.merge jobs.map { |pk| pk[:job_id] }
-      push_jobs(*jobs)
-
-      @last_polled_at      = Time.now
-      @last_poll_satisfied = count == jobs.count
-    end
-
-    def time_until_next_poll
-      wait = @poll_interval - (Time.now - @last_polled_at)
-      wait > 0 ? wait : 0
+    def unlock_jobs(ids)
+      ids.each do |id|
+        Que.execute "SELECT pg_advisory_unlock($1)", [id]
+        @locks.delete(id)
+      end
     end
   end
 end
