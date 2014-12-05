2ac954218a3dfd5d44f7a5b1a7407c86f83d98b2
Chris Hanks <christopher.m.hanks@gmail.com>
2014-12-05 15:13:29 -0500

    Fix documentation around recurring jobs. Closes #57.

--- spec/unit/customization_spec.rb
+++ spec/unit/customization_spec.rb
@@ -4,20 +4,57 @@ require 'spec_helper'
 # stay functional.
 describe "Customizing Que" do
   it "Cron should allow for easy recurring jobs" do
-    class Cron < Que::Job
-      def run
-        destroy
-        self.class.enqueue :run_at => @attrs[:run_at] + 3600
+    begin
+      class CronJob < Que::Job
+        # Default repetition interval in seconds. Can be overridden in
+        # subclasses. Can use 1.minute if using Rails.
+        INTERVAL = 60
+
+        attr_reader :start_at, :end_at, :run_again_at, :time_range
+
+        def _run
+          args = attrs[:args].first
+          @start_at, @end_at = Time.at(args.delete('start_at')), Time.at(args.delete('end_at'))
+          @run_again_at = @end_at + self.class::INTERVAL
+          @time_range = @start_at...@end_at
+
+          super
+
+          args['start_at'] = @end_at.to_f
+          args['end_at']   = @run_again_at.to_f
+          self.class.enqueue(args, run_at: @run_again_at)
+        end
       end
+
+      class MyCronJob < CronJob
+        INTERVAL = 1.5
+
+        def run(args)
+          $args       = args.dup
+          $time_range = time_range
+        end
+      end
+
+      t = Time.now - 1000
+      MyCronJob.enqueue :start_at => t.to_f, :end_at => t.to_f + 1.5, :arg => 4
+
+      $args.should be nil
+      $time_range.should be nil
+
+      Que::Job.work
+
+      $args.should == {'arg' => 4}
+      $time_range.begin.to_f.should == t.to_f
+      $time_range.end.to_f.should == t.to_f + 1.5
+      $time_range.exclude_end?.should be true
+
+      DB[:que_jobs].get(:run_at).to_f.should be_within(0.000001).of(t.to_f + 3.0)
+      args = JSON.parse(DB[:que_jobs].get(:args))
+      args.should == [{'arg' => 4, 'start_at' => t.to_f + 1.5, 'end_at' => t.to_f + 3.0}]
+    ensure
+      $args       = nil
+      $time_range = nil
     end
-
-    Cron.enqueue
-
-    run_at = DB[:que_jobs].get(:run_at).to_f
-
-    Que::Job.work
-
-    DB[:que_jobs].get(:run_at).to_f.should be_within(0.000001).of(run_at + 3600)
   end
 
   it "Object#delay should allow for simpler job enqueueing" do
