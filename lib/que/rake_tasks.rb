d9017d9908d4f5b8953d106bca08c302c21d42db
Chris Hanks <christopher.m.hanks@gmail.com>
2015-03-12 13:25:13 -0400

    Have the rake task use whatever logger is configured, rather than forcing the use of STDOUT.

--- lib/que/rake_tasks.rb
+++ lib/que/rake_tasks.rb
@@ -1,9 +1,6 @@
 namespace :que do
   desc "Process Que's jobs using a worker pool"
   task :work => :environment do
-    require 'logger'
-
-    Que.logger        = Logger.new(STDOUT)
     Que.logger.level  = Logger.const_get((ENV['QUE_LOG_LEVEL'] || 'INFO').upcase)
     Que.worker_count  = (ENV['QUE_WORKER_COUNT'] || 4).to_i
     Que.wake_interval = (ENV['QUE_WAKE_INTERVAL'] || 0.1).to_f
