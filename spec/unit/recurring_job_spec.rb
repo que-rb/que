f4ed46807ae9ea8efe919cc17b938bca41ea6c8f
Chris Hanks <christopher.m.hanks@gmail.com>
2015-03-20 20:40:53 -0400

    Add pending specs for RecurringJob functionality.

--- spec/unit/recurring_job_spec.rb
+++ spec/unit/recurring_job_spec.rb
@@ -1,65 +1,83 @@
 require 'spec_helper'
 
 describe Que::RecurringJob do
-  it "should allow for easy recurring jobs" do
-    pending
+  # it "should allow for easy recurring jobs" do
+  #   pending
 
-    begin
-      class CronJob < Que::Job
-        # Default repetition interval in seconds. Can be overridden in
-        # subclasses. Can use 1.minute if using Rails.
-        INTERVAL = 60
+  #   begin
+  #     class CronJob < Que::Job
+  #       # Default repetition interval in seconds. Can be overridden in
+  #       # subclasses. Can use 1.minute if using Rails.
+  #       INTERVAL = 60
 
-        attr_reader :start_at, :end_at, :run_again_at, :time_range
+  #       attr_reader :start_at, :end_at, :run_again_at, :time_range
 
-        def _run
-          args = attrs[:args].first
-          @start_at, @end_at = Time.at(args.delete('start_at')), Time.at(args.delete('end_at'))
-          @run_again_at = @end_at + self.class::INTERVAL
-          @time_range = @start_at...@end_at
+  #       def _run
+  #         args = attrs[:args].first
+  #         @start_at, @end_at = Time.at(args.delete('start_at')), Time.at(args.delete('end_at'))
+  #         @run_again_at = @end_at + self.class::INTERVAL
+  #         @time_range = @start_at...@end_at
 
-          super
+  #         super
 
-          args['start_at'] = @end_at.to_f
-          args['end_at']   = @run_again_at.to_f
-          self.class.enqueue(args, run_at: @run_again_at)
-        end
-      end
+  #         args['start_at'] = @end_at.to_f
+  #         args['end_at']   = @run_again_at.to_f
+  #         self.class.enqueue(args, run_at: @run_again_at)
+  #       end
+  #     end
 
-      class MyCronJob < CronJob
-        INTERVAL = 1.5
+  #     class MyCronJob < CronJob
+  #       INTERVAL = 1.5
 
-        def run(args)
-          $args       = args.dup
-          $time_range = time_range
-        end
-      end
+  #       def run(args)
+  #         $args       = args.dup
+  #         $time_range = time_range
+  #       end
+  #     end
 
-      t = (Time.now - 1000).to_f.round(6)
-      MyCronJob.enqueue :start_at => t, :end_at => t + 1.5, :arg => 4
+  #     t = (Time.now - 1000).to_f.round(6)
+  #     MyCronJob.enqueue :start_at => t, :end_at => t + 1.5, :arg => 4
 
-      $args.should be nil
-      $time_range.should be nil
+  #     $args.should be nil
+  #     $time_range.should be nil
 
-      locker = Que::Locker.new
-      sleep_until { DB[:que_jobs].get(:run_at).to_f > t }
-      DB[:que_jobs].get(:run_at).to_f.should be_within(0.000001).of(t + 1.5)
-      locker.stop
+  #     locker = Que::Locker.new
+  #     sleep_until { DB[:que_jobs].get(:run_at).to_f > t }
+  #     DB[:que_jobs].get(:run_at).to_f.should be_within(0.000001).of(t + 1.5)
+  #     locker.stop
 
-      $args.should == {'arg' => 4}
-      $time_range.begin.to_f.round(6).should be_within(0.000001).of t
-      $time_range.end.to_f.round(6).should be_within(0.000001).of t + 1.5
-      $time_range.exclude_end?.should be true
+  #     $args.should == {'arg' => 4}
+  #     $time_range.begin.to_f.round(6).should be_within(0.000001).of t
+  #     $time_range.end.to_f.round(6).should be_within(0.000001).of t + 1.5
+  #     $time_range.exclude_end?.should be true
 
-      DB[:que_jobs].get(:run_at).to_f.round(6).should be_within(0.000001).of(t + 3.0)
-      args = JSON.parse(DB[:que_jobs].get(:args)).first
-      args.keys.should == ['arg', 'start_at', 'end_at']
-      args['arg'].should == 4
-      args['start_at'].should be_within(0.000001).of(t + 1.5)
-      args['end_at'].should be_within(0.000001).of(t + 3.0)
-    ensure
-      $args       = nil
-      $time_range = nil
-    end
-  end
+  #     DB[:que_jobs].get(:run_at).to_f.round(6).should be_within(0.000001).of(t + 3.0)
+  #     args = JSON.parse(DB[:que_jobs].get(:args)).first
+  #     args.keys.should == ['arg', 'start_at', 'end_at']
+  #     args['arg'].should == 4
+  #     args['start_at'].should be_within(0.000001).of(t + 1.5)
+  #     args['end_at'].should be_within(0.000001).of(t + 3.0)
+  #   ensure
+  #     $args       = nil
+  #     $time_range = nil
+  #   end
+  # end
+
+  it "should make the time range methods available to the run method"
+
+  it "should reenqueue by default with the same arguments"
+
+  it "should support being reenqueued in a transaction"
+
+  it "should reenqueue itself if it wasn't reenqueued already"
+
+  it "should allow its arguments to be overridden when reenqueued"
+
+  it "should respect the @interval configuration"
+
+  it "should allow @interval to be overridden in subclasses as you would expect"
+
+  it "should throw an error on the initial enqueueing if an @interval is not set"
+
+  it "should throw an error on the reenqueueing if an @interval is not set"
 end
