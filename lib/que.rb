838cb8627738481d7aa268900fadb4f9d386a823
Joe Van Dyk <joe@tanga.com>
2015-09-03 10:09:43 -0700

    Don't copy ActiveSupport's constantize, just use their version if it exists.

--- lib/que.rb
+++ lib/que.rb
@@ -129,35 +129,12 @@ module Que
       @disable_prepared_statements || false
     end
 
-    # Copied from ActiveSupport's 4.2.4 version.
     def constantize(camel_cased_word)
-      names = camel_cased_word.split('::')
-
-      # Trigger a built-in NameError exception including the ill-formed constant in the message.
-      Object.const_get(camel_cased_word) if names.empty?
-
-      # Remove the first blank element in case of '::ClassName' notation.
-      names.shift if names.size > 1 && names.first.empty?
-
-      names.inject(Object) do |constant, name|
-        if constant == Object
-          constant.const_get(name)
-        else
-          candidate = constant.const_get(name)
-          next candidate if constant.const_defined?(name, false)
-          next candidate unless Object.const_defined?(name)
-
-          # Go down the ancestors to check if it is owned directly. The check
-          # stops when we reach Object or the end of ancestors tree.
-          constant = constant.ancestors.inject do |const, ancestor|
-            break const    if ancestor == Object
-            break ancestor if ancestor.const_defined?(name, false)
-            const
-          end
-
-          # owner is in Object, so raise
-          constant.const_get(name, false)
-        end
+      if camel_cased_word.respond_to?(:constantize)
+        # Use ActiveSupport's version if it exists.
+        camel_cased_word.constantize
+      else
+        string.split('::').inject(Object, &:const_get)
       end
     end
 
