d97019fa1eb8a396528e81d7b9cd863c328f7196
Chris Hanks <christopher.m.hanks@gmail.com>
2015-10-16 16:22:24 -0400

    Stop sequestering ActiveRecord specs.

--- spec/adapters/active_record_spec.rb
+++ spec/adapters/active_record_spec.rb
@@ -1,66 +1,62 @@
-# Don't run these specs in JRuby until jruby-pg is compatible with ActiveRecord.
-unless defined?(RUBY_ENGINE) && RUBY_ENGINE == 'jruby'
+require 'spec_helper'
+require 'active_record'
 
-  require 'spec_helper'
-  require 'active_record'
+if ActiveRecord.version.release >= Gem::Version.new('4.2')
+  ActiveRecord::Base.raise_in_transactional_callbacks = true
+end
+ActiveRecord::Base.establish_connection(QUE_URL)
 
-  if ActiveRecord.version.release >= Gem::Version.new('4.2')
-    ActiveRecord::Base.raise_in_transactional_callbacks = true
-  end
-  ActiveRecord::Base.establish_connection(QUE_URL)
+Que.connection = ActiveRecord
+QUE_POOLS[:active_record] = Que.pool
 
-  Que.connection = ActiveRecord
-  QUE_POOLS[:active_record] = Que.pool
+describe "Que using the ActiveRecord pool" do
+  before { Que.pool = QUE_POOLS[:active_record] }
 
-  describe "Que using the ActiveRecord pool" do
-    before { Que.pool = QUE_POOLS[:active_record] }
+  it_behaves_like "a Que pool"
 
-    it_behaves_like "a Que pool"
-
-    it "should use the same connection that ActiveRecord does" do
-      begin
-        class ActiveRecordJob < Que::Job
-          def run
-            $pid1 = Que.execute("SELECT pg_backend_pid()").first[:pg_backend_pid]
-            $pid2 = Integer(ActiveRecord::Base.connection.select_value("SELECT pg_backend_pid()"))
-          end
+  it "should use the same connection that ActiveRecord does" do
+    begin
+      class ActiveRecordJob < Que::Job
+        def run
+          $pid1 = Que.execute("SELECT pg_backend_pid()").first[:pg_backend_pid]
+          $pid2 = Integer(ActiveRecord::Base.connection.select_value("SELECT pg_backend_pid()"))
         end
-
-        ActiveRecordJob.enqueue
-        locker = Que::Locker.new
-
-        sleep_until { Integer === $pid1 && Integer === $pid2 }
-        $pid1.should == $pid2
-      ensure
-        $pid1 = $pid2 = nil
-        locker.stop if locker
       end
+
+      ActiveRecordJob.enqueue
+      locker = Que::Locker.new
+
+      sleep_until { Integer === $pid1 && Integer === $pid2 }
+      $pid1.should == $pid2
+    ensure
+      $pid1 = $pid2 = nil
+      locker.stop if locker
     end
+  end
 
-    it "should support Rails' special extensions for times" do
-      locker = Que::Locker.new poll_interval: 0.005.seconds
-      sleep 0.01
+  it "should support Rails' special extensions for times" do
+    locker = Que::Locker.new poll_interval: 0.005.seconds
+    sleep 0.01
 
-      run_at = Que::Job.enqueue(run_at: 1.minute.ago).attrs[:run_at]
-      run_at.should be_within(3).of(Time.now - 60)
+    run_at = Que::Job.enqueue(run_at: 1.minute.ago).attrs[:run_at]
+    run_at.should be_within(3).of(Time.now - 60)
 
-      sleep_until { DB[:que_jobs].empty? }
-      locker.stop
+    sleep_until { DB[:que_jobs].empty? }
+    locker.stop
+  end
+
+  it "should be able to survive an ActiveRecord::Rollback without raising an error" do
+    ActiveRecord::Base.transaction do
+      Que::Job.enqueue
+      raise ActiveRecord::Rollback, "Call tech support!"
     end
+    DB[:que_jobs].count.should be 0
+  end
 
-    it "should be able to survive an ActiveRecord::Rollback without raising an error" do
-      ActiveRecord::Base.transaction do
-        Que::Job.enqueue
-        raise ActiveRecord::Rollback, "Call tech support!"
-      end
-      DB[:que_jobs].count.should be 0
-    end
-
-    it "should be able to tell when it's in an ActiveRecord transaction" do
-      Que.should_not be_in_transaction
-      ActiveRecord::Base.transaction do
-        Que.should be_in_transaction
-      end
+  it "should be able to tell when it's in an ActiveRecord transaction" do
+    Que.should_not be_in_transaction
+    ActiveRecord::Base.transaction do
+      Que.should be_in_transaction
     end
   end
 end
