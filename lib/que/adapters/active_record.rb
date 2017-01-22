40b8d4cf6b7cfeaacb519839eb39d6b52927f47c
Chris Hanks <christopher.m.hanks@gmail.com>
2017-01-22 10:22:39 -0500

    Use Rails application's executor to protect connections we check out, when necessary.

--- lib/que/adapters/active_record.rb
+++ lib/que/adapters/active_record.rb
@@ -59,7 +59,16 @@ module Que
       private
 
       def checkout_activerecord_adapter(&block)
-        ::ActiveRecord::Base.connection_pool.with_connection(&block)
+        # Use Rails' executor (if present) to make sure that the connection
+        # we're using isn't taken from us while the block runs. See
+        # https://github.com/chanks/que/issues/166#issuecomment-274218910
+        if defined?(Rails.application.executor)
+          Rails.application.executor.wrap do
+            ::ActiveRecord::Base.connection_pool.with_connection(&block)
+          end
+        else
+          ::ActiveRecord::Base.connection_pool.with_connection(&block)
+        end
       end
     end
   end
