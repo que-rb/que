e512d74e2485e7937a6c44bd08d4ae58a862b7ed
Chris Hanks <christopher.m.hanks@gmail.com>
2017-06-16 15:09:37 -0400

    Add some pending specs for the Listener class.

---
+++ spec/unit/listener_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Que::Listener do
+  it "should return messages to the locker in bulk by type"
+
+  it "should pre-process new_job messages"
+
+  it "should be resilient to messages that aren't invalid JSON"
+
+  it "when unlistening should not leave any residual messages"
+end
