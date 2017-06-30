4352c5c62d995f8250ff0358c5cd476008bb1a6c
Chris Hanks <christopher.m.hanks@gmail.com>
2017-06-30 19:10:59 -0400

    Spec that the run_synchronously accessor works on subclasses.

--- lib/que/job.run_synchronously.spec.rb
+++ lib/que/job.run_synchronously.spec.rb
@@ -2,17 +2,37 @@
 
 require 'spec_helper'
 
-describe Que, "run_synchronously=" do
-  before { Que::Job.run_synchronously = true  }
-  after  { Que::Job.run_synchronously = false }
+describe Que::Job, "run_synchronously=" do
+  describe "on the base class" do
+    before { Que::Job.run_synchronously = true }
+    after  { Que::Job.remove_instance_variable(:@run_synchronously) }
 
-  it "should cause jobs to be worked synchronously" do
-    assert_instance_of ArgsJob, ArgsJob.enqueue(1, 2, 3)
-    assert_equal [1, 2, 3], $passed_args
+    it "should cause jobs to be worked synchronously" do
+      assert_instance_of ArgsJob, ArgsJob.enqueue(1, 2, 3)
+      assert_equal [1, 2, 3], $passed_args
+    end
+
+    it "should ignore jobs that are scheduled for a future date" do
+      assert_instance_of ArgsJob, ArgsJob.enqueue(1, 2, 3, run_at: Time.now + 3)
+      assert_nil $passed_args
+    end
   end
 
-  it "should ignore jobs that are scheduled for a future date" do
-    assert_instance_of ArgsJob, ArgsJob.enqueue(1, 2, 3, run_at: Time.now + 3)
-    assert_nil $passed_args
+  describe "on a subclass" do
+    before { ArgsJob.run_synchronously = true }
+    after  { ArgsJob.remove_instance_variable(:@run_synchronously) }
+
+    it "should cause jobs of that subclass to be worked synchronously" do
+      assert_instance_of ArgsJob, ArgsJob.enqueue(1, 2, 3)
+      assert_equal [1, 2, 3], $passed_args
+
+      job = BlockJob.enqueue(1, 2, 3)
+      assert_equal [job.que_attrs[:id]], jobs.select_map(:id)
+    end
+
+    it "should ignore jobs that are scheduled for a future date" do
+      assert_instance_of ArgsJob, ArgsJob.enqueue(1, 2, 3, run_at: Time.now + 3)
+      assert_nil $passed_args
+    end
   end
 end
