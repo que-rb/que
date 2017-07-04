164986d553b89de7c97456344d35d34fc519ecbf
Chris Hanks <christopher.m.hanks@gmail.com>
2017-07-04 17:35:47 -0400

    Log loudly when the error_notifier proc fails.

--- lib/que/utils/error_notification.rb
+++ lib/que/utils/error_notification.rb
@@ -10,7 +10,13 @@ module Que
           notifier.call(*args)
         end
       rescue => error
-        # Log loudly and swallow the error.
+        Que.log(
+          level: :error,
+          message: "error_notifier callable raised an error",
+          error_message: error.message,
+          error_backtrace: error.backtrace,
+        )
+        nil
       end
     end
   end
--- lib/que/utils/error_notification.spec.rb
+++ lib/que/utils/error_notification.spec.rb
@@ -4,17 +4,41 @@ require 'spec_helper'
 
 describe Que::Utils::ErrorNotification do
   describe "notify_error" do
-    it "should pass the args to the error notification proc"
+    it "should pass the args to the error notification proc" do
+      passed = nil
+      Que.error_notifier = proc { |*args| passed = args; :blah }
 
-    it "when there is no error notification proc should do nothing"
+      assert_equal :blah, Que.notify_error(1)
+      assert_equal [1], passed
+
+      assert_equal :blah, Que.notify_error(1, 2, 3)
+      assert_equal [1, 2, 3], passed
+    end
+
+    it "when there is no error notification proc should do nothing" do
+      Que.error_notifier = nil
+      assert_nil Que.notify_error(1)
+    end
 
     describe "when the error notification proc raises an error" do
-      it "should log loudly and swallow it"
+      it "should log loudly and swallow it" do
+        Que.error_notifier = proc { |*args| raise "Uh-oh!" }
+        assert_nil Que.notify_error(1)
+
+        assert_equal 1, logged_messages.length
+        m = logged_messages.first
+
+        assert_equal "error_notifier callable raised an error", m['message']
+        assert_equal "Uh-oh!", m['error_message']
+        assert_instance_of Array, m['error_backtrace']
+      end
     end
   end
 
   describe "notify_error_async" do
     it "should invoke notify_error in a separate thread"
+
+    it "should swallow the error if there are more than five in the queue"
   end
 
   describe "error_notifier=" do
