a8e0ab0b91e3fb8436eea5516e167eee86634b5b
Chris Hanks <christopher.m.hanks@gmail.com>
2017-07-03 22:05:32 -0400

    Add specs for create! and drop!

--- lib/que/utils/queue_management.spec.rb
+++ lib/que/utils/queue_management.spec.rb
@@ -13,10 +13,21 @@ describe Que::Utils::QueueManagement do
   end
 
   describe "create!" do
-    it "should migrate the job queue to version #1"
+    it "should migrate the job queue to version #1" do
+      Que.migrate! version: 0
+      assert_equal 0, Que.db_version
+      Que.create!
+      assert_equal 1, Que.db_version
+      Que.migrate!
+    end
   end
 
   describe "drop!" do
-    it "should drop the job queue entirely"
+    it "should drop the job queue entirely" do
+      refute_equal 0, Que.db_version
+      Que.drop!
+      assert_equal 0, Que.db_version
+      Que.migrate!
+    end
   end
 end
