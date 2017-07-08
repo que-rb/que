310431cb20790192d66ee0c905b15d883c1cb8e0
Chris Hanks <christopher.m.hanks@gmail.com>
2017-07-08 19:16:46 -0400

    Revert "Add a failing spec so we try to catch it."
    This reverts commit 51b8f2ce56e090278a270facf3f948a670d19644.
    
    Conflicts:
    	.circleci/config.yml

--- lib/que/connection_pool.spec.rb
+++ lib/que/connection_pool.spec.rb
@@ -8,10 +8,6 @@ describe Que::ConnectionPool do
   end
 
   describe ".checkout" do
-    it "failing spec" do
-      raise "Blah!"
-    end
-
     it "should yield a connection" do
       pool.checkout do |conn|
         assert_instance_of PG::Connection, conn
