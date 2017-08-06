4be4941fc1b42abe47d49676e4d9e4f099ee7aed
Chris Hanks <christopher.m.hanks@gmail.com>
2017-08-06 10:43:10 -0400

    Drop support for waking a worker in the same process after enqueuing a job in an ActiveRecord transaction.

--- lib/que/adapters/active_record.rb
+++ lib/que/adapters/active_record.rb
@@ -7,15 +7,6 @@ module Que
         checkout_activerecord_adapter { |conn| yield conn.raw_connection }
       end
 
-      def wake_worker_after_commit
-        # Works with ActiveRecord 3.2 and 4 (possibly earlier, didn't check)
-        if in_transaction?
-          checkout_activerecord_adapter { |adapter| adapter.add_transaction_record(TransactionCallback.new) }
-        else
-          Que.wake!
-        end
-      end
-
       def cleanup!
         # ActiveRecord will check out connections to the current thread when
         # queries are executed and not return them to the pool until
@@ -28,32 +19,6 @@ module Que
         # the additional step of telling ActiveRecord to check in all of the
         # current thread's connections between jobs.
         ::ActiveRecord::Base.clear_active_connections!
-      end
-
-      class TransactionCallback
-        def has_transactional_callbacks?
-          true
-        end
-
-        def rolledback!(force_restore_state = false, should_run_callbacks = true)
-          # no-op
-        end
-
-        def committed!(should_run_callbacks = true)
-          Que.wake!
-        end
-
-        def before_committed!(*)
-          # no-op
-        end
-
-        def add_to_transaction
-          # no-op.
-          #
-          # This is called when we're in a nested transaction. Ideally we would
-          # `wake!` when the outer transaction gets committed, but that would be
-          # a bigger refactor!
-        end
       end
 
       private
