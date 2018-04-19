59c249b15b6af8ec3276e98b3b9b6ff3323a7a27
Chris Hanks <christopher.m.hanks@gmail.com>
2018-04-19 16:27:48 -0400

    Drop broken code from Railtie.

--- lib/que/rails/railtie.rb
+++ lib/que/rails/railtie.rb
@@ -5,8 +5,6 @@ module Que
     class Railtie < ::Rails::Railtie
       config.que = Que
 
-      Que.run_asynchronously = true if ::Rails.env.test?
-
       Que.logger     = proc { ::Rails.logger }
       Que.connection = ::ActiveRecord if defined? ::ActiveRecord
     end
