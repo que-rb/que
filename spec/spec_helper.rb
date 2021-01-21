74981986ad4fdbf4d848c9bdd92564b402d20ea8
Oliver Morgan <ollym@me.com>
2021-01-21 15:45:48 +0100

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -192,8 +192,8 @@ class QueSpec < Minitest::Spec
     end || raise("sleep_until_equal: expected #{expected.inspect}, got #{actual.inspect}")
   end
 
-  def sleep_until(*args, &block)
-    sleep_until?(*args, &block) || raise("sleep_until timeout reached")
+  def sleep_until(**args, &block)
+    sleep_until?(**args, &block) || raise("sleep_until timeout reached")
   end
 
   def sleep_until?(timeout: SLEEP_UNTIL_TIMEOUT)
