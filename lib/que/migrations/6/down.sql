b0581ff42b303a5d05f29d381100438c70b6667a
Maddy Markovitz <maddy.markovitz@greensync.com.au>
2022-02-22 16:37:17 +1100

    Add a GIN index on kwargs column

--- lib/que/migrations/6/down.sql
+++ lib/que/migrations/6/down.sql
@@ -1,3 +1,5 @@
+DROP INDEX que_jobs_kwargs_gin_idx;
+
 ALTER TABLE que_jobs
   DROP COLUMN kwargs;
 
--- lib/que/migrations/6/up.sql
+++ lib/que/migrations/6/up.sql
@@ -1,5 +1,7 @@
 ALTER TABLE que_jobs
   ADD COLUMN kwargs JSONB NOT NULL DEFAULT '{}';
 
+CREATE INDEX que_jobs_kwargs_gin_idx ON que_jobs USING gin (kwargs jsonb_path_ops);
+
 ALTER TABLE que_jobs
   ALTER COLUMN job_schema_version DROP DEFAULT;
