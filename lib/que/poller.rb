9848fa5ecf697a2782a968e15bd719fded6f3a44
Brendan Weibrecht <brendan@weibrecht.net.au>
2022-02-23 22:00:25 +1100

    Use Que major version number in job polling query dynamically

--- lib/que/poller.rb
+++ lib/que/poller.rb
@@ -68,7 +68,7 @@ module Que
             SELECT j
             FROM public.que_jobs AS j
             WHERE queue = $1::text
-              AND que_version = 1
+              AND que_version = #{Gem::Version.new(Que::VERSION).segments.first}
               AND NOT id = ANY($2::bigint[])
               AND priority <= pg_temp.que_highest_remaining_priority($3::jsonb)
               AND run_at <= now()
@@ -89,7 +89,7 @@ module Que
                   SELECT j
                   FROM public.que_jobs AS j
                   WHERE queue = $1::text
-                    AND que_version = 1
+                    AND que_version = #{Gem::Version.new(Que::VERSION).segments.first}
                     AND NOT id = ANY($2::bigint[])
                     AND priority <= pg_temp.que_highest_remaining_priority(jobs.remaining_priorities)
                     AND run_at <= now()
