f0a290e2a07d57ec46d59324f2d2cd9c92f6e794
Brendan Weibrecht <brendan@weibrecht.net.au>
2022-02-25 17:42:15 +1100

    Drop outdated index: que_poll_idx
    This one was used prior to v1.3.0, before polling needed to take into account `job_schema_version`

--- lib/que/migrations/6/down.sql
+++ lib/que/migrations/6/down.sql
@@ -1,3 +1,4 @@
+CREATE INDEX que_poll_idx ON que_jobs (queue, priority, run_at, id) WHERE (finished_at IS NULL AND expired_at IS NULL);
 DROP INDEX que_jobs_kwargs_gin_idx;
 
 ALTER TABLE que_jobs
--- lib/que/migrations/6/up.sql
+++ lib/que/migrations/6/up.sql
@@ -2,6 +2,7 @@ ALTER TABLE que_jobs
   ADD COLUMN kwargs JSONB NOT NULL DEFAULT '{}';
 
 CREATE INDEX que_jobs_kwargs_gin_idx ON que_jobs USING gin (kwargs jsonb_path_ops);
+DROP INDEX que_poll_idx;
 
 ALTER TABLE que_jobs
   ALTER COLUMN job_schema_version DROP DEFAULT;
