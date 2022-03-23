7bc3cf8fd8b78172817e2e70ec8979bd4c24411e
Brendan Weibrecht <brendan@weibrecht.net.au>
2022-03-24 02:01:09 +1100

    Remove line wrapping in latest migration

--- lib/que/migrations/6/down.sql
+++ lib/que/migrations/6/down.sql
@@ -1,8 +1,6 @@
 CREATE INDEX que_poll_idx ON que_jobs (queue, priority, run_at, id) WHERE (finished_at IS NULL AND expired_at IS NULL);
 DROP INDEX que_jobs_kwargs_gin_idx;
 
-ALTER TABLE que_jobs
-  DROP COLUMN kwargs;
+ALTER TABLE que_jobs DROP COLUMN kwargs;
 
-ALTER TABLE que_jobs
-  ALTER COLUMN job_schema_version SET DEFAULT 1;
+ALTER TABLE que_jobs ALTER COLUMN job_schema_version SET DEFAULT 1;
